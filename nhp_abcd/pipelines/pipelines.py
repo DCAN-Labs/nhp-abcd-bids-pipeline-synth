c05591fcfac3d15e70f8fe3777904a9ce6032d89
Thomas Madison <56737848+madisoth@users.noreply.github.com>
2025-09-25 14:42:28 -0700

    Modify tasklist command for fmriset matching
    Updated tasklist command to include underscores around fmriset for better matching.

--- nhp_abcd/pipelines/pipelines.py
+++ nhp_abcd/pipelines/pipelines.py
@@ -1095,8 +1095,10 @@ class DCANBOLDProcessing(Stage):
         cmd += " --teardown"
 
         for fmriset in fmrisets:
-            fmrilist = sorted([fmri for fmri in fmris if fmriset in fmri])
-            cmd += " --tasklist " + ",".join(fmrilist)
+            # add enclosing underscores to fmriset (the BIDS task entity) to prevent a match in the case bids_task is a 
+            # substring of another task present in the session, e.g. "task-rest" and "task-rest2"
+            fmrilist = sorted([fmri for fmri in fmris if "_" + fmriset + "_" in fmri])
+            cmd += ' --tasklist ' + ','.join(fmrilist)
 
         log_dir = self._get_log_dir()
         out_log = os.path.join(log_dir, self.__class__.__name__ + "_teardown.out")
